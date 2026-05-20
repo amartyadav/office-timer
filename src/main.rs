869a10231c1cfc8d5a8092ea6139792a90e98a68
Amartya <amartya.yadav@calligotech.com>
2026-05-20 09:46:13 +0530

    commented out notif and added time input from cli

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use notify_rust::{Notification, Timeout};
 
 fn main() {
-    Notification::new()
-    .summary("Firefox News")
-    .body("This will almost look like a real firefox notification.")
-    .icon("firefox")
-    .timeout(Timeout::Milliseconds(6000)) //milliseconds
-    .show().unwrap();
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        let time: &String = &args[1];
+        println!("Checked in at: {}", time);
+    }
+
+    /* Notification::new()
+     .summary("Firefox News")
+     .body("This will almost look like a real firefox notification.")
+     .icon("firefox")
+     .timeout(Timeout::Milliseconds(6000)) //milliseconds
+     .show().unwrap();
+    */
 }
